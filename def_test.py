bbeb13e8de99d8f722ad441b78ad2b9428a583f2
jeongwon6955 <choirio2467@gmail.com>
2025-10-30 09:47:40 +0900

    .

---
+++ def_test.py
@@ -0,0 +1,29 @@
+# 함수
+
+# def hello():
+#     print('안녕하세요')
+#     print('인평자동차고')
+#     print('최정원입니다')
+
+# hello()
+# hello()
+# hello()
+# hello()
+# hello()
+
+# 매개변수
+
+# def hello(a): # 매개변수
+#     print(f'안녕 {a}아!!! 반가워!!!')
+
+# hello('정원') # 인수
+# hello('아이유')
+# hello('인평')
+
+def hello(a,b,c,d):
+    print(f'반가워 {a}아!!!')
+    print(f'반가워 {b}아!!!')
+    print(f'반가워 {c}아!!!')
+    print(f'반가워 {d}아!!!')
+
+hello('은준','준상','세현','종윤')
