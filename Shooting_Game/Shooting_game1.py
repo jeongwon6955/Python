ef18aa440aa2a8153207f6db2c90a6bdf7ae5be4
jeongwon6955 <choirio2467@gmail.com>
2025-11-27 15:27:10 +0900

    .

--- Shooting_Game/Shooting_game1.py
+++ Shooting_Game/Shooting_game1.py
@@ -30,18 +30,40 @@ def img_load(obj):
 # ---------------------------------------------
 
 
-bg = img_load("background1.jpg")[0] # 이미지 가져오기
+bg = img_load("background2.jpg")[0] # 이미지 가져오기
 p,pw,ph = img_load("plane3.png")
-r,rw,rh = img_load("rock02.png")
-
 px = w/2-pw/2
 py = h-ph
 ps = 0 # 전투기 스피드
 
-pad.blit(bg,(-100,-100))
-pad.blit(p,(px,py))
-pad.blit(r,(random.randint(0,w-rw),10))
+# -----------------운석그리기----------------
+rlist = ["rock01", "rock02", "rock03", "rock04", "rock05", "rock06", "rock07", "rock08", "rock09", "rock10",]
 
+r,rw,rh = img_load(random.choice(rlist)+".png")
+rx = random.randint(0,w-rw)
+ry = 10
+rs = 2
+
+def rock_init():
+    global r, rw, rh, rx, ry
+    r,rw,rh = img_load(random.choice(rlist)+".png")
+    rx = random.randint(0,w-rw)
+    ry = 10
+
+# -------------------------------------------
+
+# -----------------미사일그리기----------------
+m,mw,mh = img_load("missile.png")
+
+mx = px+pw/2-mw/2
+my = py-mh
+mlist = []
+# -------------------------------------------
+
+pad.blit(bg,(0,0))
+pad.blit(p,(px,py))
+pad.blit(m,(mx,my))
+pad.blit(r,(rx,ry))
 
 pygame.display.update()
 
@@ -57,6 +79,11 @@ while True:
                 ps = -10
             elif event.key == pygame.K_RIGHT: # 오른쪽 화살표키
                 ps = +10
+            elif event.key == pygame.K_SPACE:
+                mx = px+pw/2-mw/2
+                my = py-mh
+                mlist.append([mx,my]) # 발사할 미사일
+
         if event.type in [pygame.KEYUP]:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 ps = 0
@@ -66,8 +93,19 @@ while True:
     elif(px >= w-pw):
         px = w-pw
     
-    pad.blit(bg,(-100,-100)) # 잔상 제거
+    ry += rs
+    if(ry > h): # 바위가 화면 아래로 떨어지면
+        rock_init()
+
+    pad.blit(bg,(0,0)) # 잔상 제거
     pad.blit(p,(px,py))
-    pad.blit(r,(200,10))
+    pad.blit(r,(rx,ry))
+
+    if len(mlist) != 0: # 발사된 미사일이 있을때
+        for mis in mlist:
+            mis[1] -= 20
+            pad.blit(m,(mis[0],mis[1]))
+            if mis[1] < -50:
+                mlist.remove(mis)
     pygame.display.update()
     clock.tick(60)
