301456ffa91b9faeea9df6826e2aaab77be5d0ea
jeongwon6955 <choirio2467@gmail.com>
2025-12-04 15:12:44 +0900

    .

--- Shooting_Game/Shooting_game1.py
+++ Shooting_Game/Shooting_game1.py
@@ -18,8 +18,27 @@ blue = (0,0,255)
 w = 480
 h = 600
 
+score = 0
+miss = 0
+
 pad = pygame.display.set_mode((w,h))
 pygame.display.set_caption('Shooting Game')
+
+# -------------게임 종료 함수-----------
+def gameover():
+    global pad, run
+    write("Game Over~!!!!",50,200,red,50)
+
+    gameover_sound.play()
+    pygame.display.update()
+    run = False
+
+# ------------사운드 로드-------------
+pygame.mixer.music.load("images/bgm.wav") # 배경음악 로드
+pygame.mixer.music.play(-1) # 배경음악 무한 재생
+mis_sound = pygame.mixer.Sound("images/missile2.mp3")
+exp_sound = pygame.mixer.Sound("images/explosion.wav")
+gameover_sound = pygame.mixer.Sound("images/gameover.wav")
 
 # -------------글씨 쓰기 함수------------------
 def write(string,a,b,color,size = 20):
@@ -80,7 +99,8 @@ pygame.display.update()
 
 clock = pygame.time.Clock()
 
-while True:
+run = True
+while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -91,6 +111,7 @@ while True:
             elif event.key == pygame.K_RIGHT: # 오른쪽 화살표키
                 ps = +10
             elif event.key == pygame.K_SPACE:
+                mis_sound.play()
                 mx = px+pw/2-mw/2
                 my = py-mh
                 mlist.append([mx,my]) # 발사할 미사일
@@ -106,6 +127,10 @@ while True:
     
     ry += rs
     if(ry > h): # 바위가 화면 아래로 떨어지면
+        miss += 1
+        rs += 0.5
+        if rs >= 10:
+            rs = 10
         rock_init()
 
     pad.blit(bg,(0,0)) # 잔상 제거
@@ -115,7 +140,12 @@ while True:
     # 전투기와 바위가 부딪친 상황
     if (py < ry + rh) and (px < rx + rw) and (px + pw > rx):
         pad.blit(exp,(rx,ry))
-        rock_init() 
+        exp_sound.play()
+        miss += 1
+        rock_init()
+        rs += 0.5 
+        if rs >= 10:
+            rs = 10
 
     if len(mlist) != 0: # 발사된 미사일이 있을때
         for mis in mlist:
@@ -123,13 +153,25 @@ while True:
             # 운석과 미사일이 충돌
             if mis[1] < ry + rh and (mis[0] < rx + rw) and (mis[0] + mw > rx):
                 pad.blit(exp,(rx, ry))
+                exp_sound.play()
                 mlist.remove(mis)
+                score += 1
                 rock_init()
             elif mis[1] < -50:
                 mlist.remove(mis)
 
             pad.blit(m,(mis[0],mis[1]))
     
-    write("Scores: ",10,10,white)
+    score_text = 'Score: '+ str(score)
+    write(f"Scores: {score}",10,10,white)
+    miss_text = 'Miss: '+ str(miss)
+    write(f"Miss: {miss}",400,10,red)
+
+    if miss >= 5:
+        gameover()
+
     pygame.display.update()
     clock.tick(60)
+
+pygame.time.delay(4000)
+pygame.quit()
